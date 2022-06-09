fd072afa38077fc55f5fad6c457e3d0723d79148
Sikorski <sikorski@zib.de>
2022-06-09 10:25:57 +0200

    working proof of concept

--- src/ociso.jl
+++ src/ociso.jl
@@ -8,12 +8,22 @@
 # we need
 # - a trajectory sampler + girsanov
 # - the eigenfunction (test this before isokann convergence)
+
+# what we learned
+# - need to adjust for time scaling of non-eigenfunction k
+# - T eigenfunctions are hard, use SQRA
+
 using Flux
 using DifferentialEquations
 using Interpolations
 using LinearAlgebra
+using Plots
+using ForwardDiff
+using Parameters
 
-grad(f, x) = collect(Flux.gradient(f, x)[1])
+#grad(f, x) = collect(Flux.gradient(f, x)[1])
+
+grad(f, x) = ForwardDiff.gradient(f, x)
 
 doublewell(x) = ((x[1])^2 - 1) ^ 2
 
@@ -25,18 +35,21 @@ function sdeproblem(dynamics=dynamics(), x0=[0.])
     prob = SDEProblem(f, g, x0, (0., dynamics.T))
 end
 
+u_star(x, k, σ) = (σ' * grad(k, x) / k(x)) :: Vector
+
 function controlled_drift(xg, p, t)
-    (;σ, k, U) = p
+    (;σ, k, U, u) = p
     x = @view xg[1:end-1]
-    u = (-σ' * grad(k, x) / k(x)) :: Vector
-    dV = (-Flux.gradient(U, x)[1]) :: Vector
-    return [dV + u; sum(abs2, u) / 2] :: Vector
+    uu = u(x)
+    du = [-grad(U, x) + uu; sum(abs2, uu) / 2]
+
+    return du
 end
 
 function controlled_noise(xg, p, t)
-    (;σ, k, Σ, n) = p
+    (;σ, k, Σ, n, u) = p
     x = @view xg[1:end-1]
-    Σ[n+1, 1:n] .= (-σ' * grad(k, x) / k(x)) :: Vector
+    Σ[n+1, 1:n] .= u(x)
     return Σ :: Matrix
 end
 
@@ -45,7 +58,17 @@ function controlled_parameters(σ, k, U)
     σ = reshape(σ, n, n)
     Σ = zeros(n+1, n+1)
     Σ[1:n, 1:n] = σ
-    return (; n, σ, Σ, k, U)
+    u = x -> u_star(x, k, σ)
+    return (; n, σ, Σ, k, U, u)
+end
+
+struct ControlledProblem
+    potential
+    sigma
+    T
+    k
+    u
+    Σ
 end
 
 controlled_problem(; σ=[1], U=doublewell, k=k1, x0 = [0.], T = 1) = controlled_problem(σ, U, k, x0, T)
@@ -63,8 +86,34 @@ function optexp(f)
     p = controlled_problem(k=k)
     s = solve(p)
     plot(s) |> display
-    f(s[end][1]) * exp(-s[end][2]) - 1 # this should be 0 variance for K[f]
+
+    k(s[end][1]) * exp(-s[end][2]) - 1 # this should be 0 variance for K[f]
+    k, p, s
 end
+
+function compare(nef = 1000, nsol = 10000)
+    T, f = eigenfunction(-2:.1:2, 2000) # compute the eigenfunction
+    k(x) =  f(x[1]) + 1 # shifted eigenfunction
+    #k(x) = f(x[1]) - minimum(f.(-3:3)) + 0.1
+    p0 = controlled_problem(ones(1,1), doublewell, x->1, 0., 1.)
+    pu = controlled_problem(ones(1,1), doublewell, k, 0., 1.)
+    sol0=[solve(p0) for i in 1:10000]
+    solu=[solve(pu) for i in 1:10000]
+
+    map([solu, sol0]) do sol
+        ex = eval_girsanov.(sol, k)
+       @show mean_and_std(ex)
+       histogram(ex)|>display
+    end
+    sol0, solu, f
+end
+
+
+
+
+eval_girsanov(s::DifferentialEquations.RODESolution, k) = eval_girsanov(s[end][1], s[end][2], k)
+
+eval_girsanov(x, g, k) = k(x) * exp(-g)
 
 ### COMPUTATION OF eigenfunction
 
@@ -98,15 +147,16 @@ end
 function eigenfunction(grid, T::Matrix)
     e = eigen(T, sortby=x->-real(x))
     v = e.vectors[:, 2] |> real
-    @show e.values[2]
-    return LinearInterpolation(grid, v, extrapolation_bc=Flat())
+    @show val = e.values[2]
+    int = CubicSplineInterpolation(grid, v, extrapolation_bc=Flat())
+    return int, v, val
 end
 
 function eigenfunction(grid=-2:.2:2, n=100, dynamics=dynamics())
     T = ulam(grid, n, dynamics, 1e-2)
-    f = eigenfunction(grid, T)
+    f, v, val = eigenfunction(grid, T)
     plot(grid, map(f, grid)) |> display
-    T, f
+    T, f, v, val
 end
 
 
