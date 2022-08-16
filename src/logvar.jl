2d48fc1d0afb98aa791076c9adb85f90ebd2d504
Sikorski <sikorski@zib.de>
2022-08-16 16:44:34 +0200

    compute the logvar adjoint manually

--- src/logvar.jl
+++ src/logvar.jl
@@ -1,23 +1,33 @@
-using StochasticDiffEq
 using LinearAlgebra
 using StaticArrays
+using Flux, Zygote
+using DifferentialEquations, StochasticDiffEq, SciMLSensitivity
+
+# control problem problem
 
 dim = 1
 sigma(X, t) = ones(dim,dim)
 Sigma(X) = similar(X, size(X).+1 ...)
 b(X, t) = zero(X)
 v(X, t) = zero(X)
-u(X, t) = zero(X)
-f(x, T) = 1
+u(X, t, p) = anneval(p, X)
+f(x, T) = 1.
 sigma(X::SVector{N, T}, t) where {N, T} = SMatrix{N, N, T}(I)
 
-""" immutable """
+# neural network modeling force u
+
+ann = Chain(Dense(1,10,tanh), Dense(10,1))
+anneval(p, X) = re(p)(X) # evaluation at X with parameters p
+p1,re = Flux.destructure(ann)
+ps = Flux.params(p1)
+
+# went for immutable SVectors since they were much faster in normal sde solve
 function drift(s::SVector, p, t)
     N = length(s) - 1
     X = s[SOneTo(N)]  # indexing like this to keep it an SVector
     let b = b(X, t),  # is this beautiful or horrible :D?
         v = v(X, t),
-        u = anneval(p, X),
+        u = u(X, t, p),
         σ = sigma(X, t),
         f = f(X, t)
 
@@ -31,33 +41,85 @@ function noise(s::SVector, p, t)
     N = length(s) - 1
     X = s[SOneTo(N)]
     ds = zero(MMatrix{N+1,N+1})
+
     ds[1:N, 1:N] = sigma(X, t)
-    ds[N+1, 1:N] .= anneval(p, X)
+    ds[N+1, 1:N] .= u(X, t, p)
+
     SMatrix(ds)
 end
 
-ann = Chain(Dense(1,10,tanh), Dense(10,1))
-anneval(p, X) = re(p)(X) # evaluation at X with parameters p
-p1,re = Flux.destructure(ann)
-ps = Flux.params(p1)
-
+# default problem
 LogVarProblem(x0=[0.], T=1., p=p1) = LogVarProblem(SVector(x0...), T, p)
 
 function LogVarProblem(x0::StaticArray, T, p)
     s = vcat(x0, 0)
     noise_proto = noise(s, p, 0)
     SDEProblem{false}(drift, noise, s, T, p, noise_rate_prototype = noise_proto)
-    ODEProblem{false}(drift, s, T, p)
+    #ODEProblem{false}(drift, s, T, p)
 end
 
+# normal solve works
 function msolve()
     p = LogVarProblem()
-    s = solve(p, dt=.001)
+    alg = isa(p, ODEProblem) ? Tsit5() : EM()
+    s = solve(p, alg, dt=.01)
 end
 
+# sensivity doesnt
 function msens()
     p = LogVarProblem()
     Zygote.gradient(()->msolve()[end][2], ps)
+end
+
+### hence we compute the adjoint / derivatives manually :|
+
+function AdjointLogVarProblem(x0::StaticArray, T, force, dudp)
+    N = length(x0)
+    M = size(dudp(x0), 2)
+    s = vcat(x0, @SVector(zeros(M)))
+
+    function ddrift(s::SVector, p, t)
+        X = s[SOneTo(N)]  # indexing like this to keep it an SVector
+
+        bb = b(X, t)  # is this beautiful or horrible :D?
+        vv = v(X, t)
+        uu = force(X)
+        ss = sigma(X, t)
+        ff = f(X, t)
+        du = dudp(X)
+
+        dX = bb + ss * vv  # v controlled process
+        dY = du' * (uu - vv)
+        vcat(dX, dY)
+
+    end
+
+    function dnoise(s, p, t)
+        X = s[SOneTo(N)]
+        ds = zero(MMatrix{N+M,N+M})
+        ds[1:N, 1:N] = sigma(X, t)
+        dd = dudp(X)
+        ds[N+1:N+M, 1:N] .= dd'
+        SMatrix(ds)
+    end
+
+    noise_proto = dnoise(s, 0, 0)
+
+    SDEProblem{false}(ddrift, dnoise, s, T, noise_rate_prototype = noise_proto)
+end
+
+
+# return dY/dp (x) where Y is the cost functional and p the ann parameters
+# next step would be to define the chain rule for this and use it in the variance minim.
+function adjoint(x=rand(1), par=p1)
+    x = SVector{length(x)}(x)
+    u(x) = re(par)(x)
+    ps = Flux.params(par)
+    dudp(x) = Zygote.jacobian(()->u(x), ps) |> first
+    dudp(x) :: AbstractMatrix
+    p = AdjointLogVarProblem(x, 1., u, dudp)
+    sol = solve(p, EM(), dt=.1)
+    sol[end][length(x)+1:end]
 end
 
 
