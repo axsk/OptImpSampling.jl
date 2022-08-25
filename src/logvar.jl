1c03080d61da7e660d2d6af0b3a0307ec03fba6b
Sikorski <sikorski@zib.de>
2022-08-25 14:13:55 +0200

    logvar: add training and terminal condition (which breaks ad)

--- src/logvar.jl
+++ src/logvar.jl
@@ -3,7 +3,7 @@ using Zygote
 using StochasticDiffEq, SciMLSensitivity
 using StatsBase
 using Lux, Random
-#using ReverseDiff
+using Optimisers
 
 # dX = b + σ * v + σ * dB
 # dY = -f - u'v + u'u/2
@@ -11,11 +11,14 @@ using Lux, Random
 # Motivation: The optimal u-controlled process gives is a 0-variance estimator for
 # W = ∫ f(X(t)) dt
 
-# control problem problem
-b(X, t) = zero(X)
+# problem definition
+b(X, t) = -X
 v(X, t) = zero(X)
 f(x, T) = 1.
 sigma(X, t) = collect(I(length(X)))
+ub = 1
+lb = -1
+stoptime = true # XXX This does not work with autodiff :(
 
 function mydense(in=1)
     m = Lux.Chain(Lux.Dense(in,10,tanh), Lux.Dense(10,1))
@@ -32,7 +35,7 @@ function drift(dxy, xy, t, b, sigma, u, v, f)
         σ = sigma(X, t),
         f = f(X, t)
 
-        dxy[1:end-1] .= b + σ * v
+        dxy[1:end-1] .= b .+ σ * v
         dxy[end] = - f - dot(u, v) + dot(u, u) / 2
     end
 end
@@ -45,9 +48,9 @@ function noise(dxy, xy, t, sigma, u)
 end
 
 # stop after first component of trajectory crosses lower or upper bound
-termination(ub) = ContinuousCallback((u,t,int)->(u[1]-lb) * (ub-u[1]), terminate!)
+termination(ub, lb) = ContinuousCallback((u,t,int)->(u[1]-lb) * (ub-u[1]), terminate!)
 
-function LogVarProblem(x0=[0.], T=1., luxmodel=mydense())
+function LogVarProblem(x0=[0.], T=10., luxmodel=mydense())
     model, ps, st = luxmodel
     xy0 = vcat(x0, 0.)
     n0 = zeros(length(xy0), length(xy0))
@@ -57,30 +60,53 @@ function LogVarProblem(x0=[0.], T=1., luxmodel=mydense())
     _drift(dxy, xy, p, t) = drift(dxy, xy, t, b, sigma, u(p), v, f)
     _noise(dxy, xy, p, t) = noise(dxy, xy, t, sigma, u(p))
 
-    SDEProblem(_drift, _noise, xy0, T, p, noise_rate_prototype = n0)
+    cb = stoptime ? termination(ub, lb) : nothing
+    SDEProblem(_drift, _noise, xy0, T, p, noise_rate_prototype = n0, callback=cb)
 end
 
 function msolve(prob; ps=prob.p, dt=0.01, salg=InterpolatingAdjoint(autojacvec=ReverseDiffVJP(), noisemixing=true))
-    #prob = remake(prob, p=ps)  # this kills AD
+    #Zygote.@showgrad prob=remake(prob, p=ps)  # this kills AD
     s = solve(prob, EM(), sensealg=salg, dt=dt, p=ps)
-    s.u[end][end]
 end
 
+cost(sol) = sol[end][end]
+
 function msens(prob)  # this works
-    Zygote.gradient(ps->msolve(prob, ps=ps), prob.p)[1]
+    Zygote.gradient(ps->msolve(prob, ps=ps)|>cost, prob.p)[1]
 end
 
 function logvar(prob; ps=prob.p, n=10)  # this works
     #sum(_ -> msolve(prob, ps=ps) , 1:n)
-    var(msolve(prob, ps=ps) for i in 1:n)
+    var(cost(msolve(prob, ps=ps)) for i in 1:n)
 end
 
 function dlogvar(prob; n=10)  # finally working
     Zygote.gradient(ps->logvar(prob, ps=ps, n=n), prob.p)[1]
 end
 
-#import Base.+
-#+(::NamedTuple{(:data, :itr), Tuple{NamedTuple{(), Tuple{}}, Nothing}}, ::NamedTuple{(:data, :itr), Tuple{NamedTuple{(), Tuple{}}, Nothing}}) = (data=(;), itr=nothing)
+function train(prob, learniter=10, mciter=10)
+    params = prob.p
+    rule = Optimisers.Adam()
+    opt_state = Optimisers.setup(rule, params);  # optimiser state based on model parameters
+    for i in 1:learniter
+        lv, ∇params = withgradient(params) do p
+            logvar(prob, ps=p, n=mciter)
+        end
+        ∇params = ∇params[1]  # why is ∇params = ([grad], )
+        opt_state, params = Optimisers.update(opt_state, params, ∇params)
+        prob = remake(prob, p=params)
+        @show lv
+    end
+
+    return prob
+end
+
+function plotu(prob)
+    model, ps, st = mydense()  # TODO: this needs to be given
+    grid = -1:.1:1
+    us = model(collect(grid)', prob.p, st)[1]
+    plot(grid, us')
+end
 
 function benchmark()
     l = LogVarProblem()
@@ -89,148 +115,11 @@ function benchmark()
 end
 
 function test()
-    dlogvar(LogVarProblem())
+    l = LogVarProblem()
+    msolve(l)
+    msens(l)
+    logvar(l)
+    dlogvar(l)
+    train(l)
+    #plotu(l)
 end
-
-#=
-function test_ad_systems()
-    senses = [BacksolveAdjoint, InterpolatingAdjoint, QuadratureAdjoint,
-    ReverseDiffAdjoint,
-    ForwardDiffSensitivity,
-    ForwardSensitivity,
-    ZygoteAdjoint,
-    TrackerAdjoint]
-    jvs = [false, true, ZygoteVJP(), ReverseDiffVJP(true), ReverseDiffVJP(), TrackerVJP()]
-    i = 0
-    for prob in [LogVarProblem([0.]),
-        LogVarProblem(@SVector([0.])),
-        MLogVarProblem([0.]),
-        MLogVarProblem(@MVector([0.]))]
-        i+=1
-        for s in senses
-            for j in jvs
-                try
-                    @show @benchmark msens($prob, salg=$(s(autojacvec=j)))
-                    println("$i $s $j")
-                catch e
-                    println("$i  $s $j fail")
-                end
-            end
-        end
-    end
-end
-=#
-
-
-## @benchmark msens(ls::Prob{SVector, Flux.Chain}) = 37ms
-## @benchmark msens(l::Prob{Vector, Flux.Chain}) = 38ms
-## @benchmark msens(l::Prob{Vector, Lux.Chain}) = 25ms
-
-#=
-Trial(84.003 ms) oop Array InterpolatingAdjoint ZygoteVJP(false)
-Trial(23.119 ms) oop Array InterpolatingAdjoint ReverseDiffVJP{false}()
-
-Trial(48.637 ms) inplace Array BacksolveAdjoint ReverseDiffVJP{false}()
-Trial(17.011 ms) inplace Array InterpolatingAdjoint false
-Trial(23.284 ms) inplace Array InterpolatingAdjoint ReverseDiffVJP{false}()
-
-Trial(25.789 ms) inplace MArray InterpolatingAdjoint false
-Trial(32.074 ms) inplace MArray InterpolatingAdjoint ReverseDiffVJP{false}()
-=#
-
-#=
-### hence we compute the adjoint / derivatives manually :|
-
-function AdjointLogVarProblem(x0::StaticArray, T, force, dudp)
-    N = length(x0)
-    M = size(dudp(x0), 2)
-    s = vcat(x0, @SVector(zeros(M)))
-
-    function ddrift(s::SVector, p, t)
-        X = s[SOneTo(N)]  # indexing like this to keep it an SVector
-
-        bb = b(X, t)  # is this beautiful or horrible :D?
-        vv = v(X, t)
-        uu = force(X)
-        ss = sigma(X, t)
-        ff = f(X, t)
-        du = dudp(X)
-
-        dX = bb + ss * vv  # v controlled process
-        dY = du' * (uu - vv)
-        vcat(dX, dY)
-
-    end
-
-    function dnoise(s, p, t)
-        X = s[SOneTo(N)]
-        ds = zero(MMatrix{N+M,N+M})
-        ds[1:N, 1:N] = sigma(X, t)
-        dd = dudp(X)
-        ds[N+1:N+M, 1:N] .= dd'
-        SMatrix(ds)
-    end
-
-    noise_proto = dnoise(s, 0, 0)
-
-    SDEProblem{false}(ddrift, dnoise, s, T, noise_rate_prototype = noise_proto)
-end
-
-# return dY/dp (x) where Y is the cost functional and p the ann parameters
-# next step would be to define the chain rule for this and use it in the variance minim.
-function adjoint(x=rand(1), par=p1)
-    x = SVector{length(x)}(x)
-    u(x) = re(par)(x)
-    ps = Flux.params(par)
-    dudp(x) = Zygote.jacobian(()->u(x), ps) |> first
-    dudp(x) :: AbstractMatrix
-    p = AdjointLogVarProblem(x, 1., u, dudp)
-    sol = solve(p, EM(), dt=.1)
-    sol[end][length(x)+1:end]
-end
-
-
-
-## backlog
-
-# use a terminal condition
-termination = ContinuousCallback((u,t,int)->u[1]-1, terminate!)
-
-function compare_static_mutating()
-    p = LogVarProblem([0.], 1.)
-    @time s = solve(p, EM(), dt=.001)
-    p = LogVarProblem(@SVector[0.], 1.)
-    @time s = solve(p, EM(), dt=.001);
-end
-
-
-""" mutating variant, was around 10x slower """
-function LogVarProblemMutating(x0, T)
-
-    function drift(ds, s, p, t)
-        X = @view s[1:end-1]
-
-        let b = b(X, t),  # is this beautiful or horrible :D?
-            v = v(X, t),
-            u = u(X, t),
-            σ = sigma(X, t),
-            f = f(X, t)
-
-            ds[1:end-1] .= b + σ * v  # v controlled process
-            ds[end]     = - f - dot(u, v) + dot(u, u) / 2
-        end
-        nothing
-    end
-
-    function noise(ds, s, p, t)
-        X = @view s[1:end-1]
-
-        dWX = ds[1:end, 1:end] .= sigma(X, t)
-        dWY = ds[end, 1:end-1] .= u(X, t)
-        nothing
-    end
-    Sigma(X) = similar(X, size(X).+1 ...)
-    SDEProblem{true}(drift, noise, [x0; 0], (0, T), p, noise_rate_prototype = Sigma(x0))
-end
-
-=#
